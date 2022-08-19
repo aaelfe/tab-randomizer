e01665370844b37aaca14aa4c1c7ef2158a96e25
Allan Elfe <aaelfe@crimson.ua.edu>
2022-08-19 18:07:27 +0100

    Added system to move a random tab to random location every alarm

--- background.js
+++ background.js
@@ -1,5 +1,23 @@
+async function queryTabs() {
+  return chrome.tabs.query({windowType:'normal', currentWindow: true}).then(tabs=>tabs)
+}
+
+let tabs
+let tabIds=[]
+queryTabs()
+  .then((t) => tabs=t)
+
 chrome.alarms.create({periodInMinutes: 0.001})
 
 chrome.alarms.onAlarm.addListener(() => {
-  console.log('ALARM')
+  moveRandomTab()
 })
+
+function moveRandomTab() {
+  let tabId=Math.floor(Math.random()*(tabs.length-1))
+  let index=Math.floor(Math.random()*(tabs.length-1))
+  chrome.tabs.move(
+    tabs[tabId].id,
+    {'index': index},
+  )
+}
